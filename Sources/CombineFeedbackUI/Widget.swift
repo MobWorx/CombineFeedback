bd7a23658a15ec1d10674566adbdfe163d18ac36
Derek X Wang <derekxinzhewang@gmail.com>
2021-03-06 03:54:39 -0800

    Add -> ViewModelWidget, OpaqueWidget and open Store.context

--- Sources/CombineFeedbackUI/Widget.swift
+++ Sources/CombineFeedbackUI/Widget.swift
@@ -18,3 +18,37 @@ public struct Widget<State, Event, Content: View>: View {
         return content(store.context)
     }
 }
+
+public struct ViewModelWidget<State, Event, Content: View>: View {
+    private let store: Store<State, Event>
+    private let content: (Store<State, Event>, Context<State, Event>) -> Content
+
+    public init(
+        store: Store<State, Event>,
+        @ViewBuilder content: @escaping (Store<State, Event>, Context<State, Event>) -> Content
+    ) {
+        self.store = store
+        self.content = content
+    }
+
+    public var body: some View {
+        return content(store, store.context)
+    }
+}
+
+public struct OpaqueWidget<Opaque, Content: View>: View {
+    private let object: Opaque
+    private let content: (Opaque) -> Content
+
+    public init(
+        object: Opaque,
+        @ViewBuilder content: @escaping (Opaque) -> Content
+    ) {
+        self.object = object
+        self.content = content
+    }
+
+    public var body: some View {
+        return content(object)
+    }
+}
